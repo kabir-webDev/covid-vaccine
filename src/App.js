61898b01cf456f2e8fed4628d80b4064e4350c61
Kabir Hasan <kabir.webdev@gmail.com>
2021-06-19 23:15:21 +0600

    Re-work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Docs from "./components/Docs";
+import Features from "./components/Features";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -9,6 +12,17 @@ function App() {
     <Router>
       <div className="App">
         <Navbar />
+        <Switch>
+          <Route path="/features">
+            <Features />
+          </Route>
+          <Route path="/docs">
+            <Docs />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
         <Footer />
       </div>
     </Router>
