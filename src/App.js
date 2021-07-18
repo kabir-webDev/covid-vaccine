fb5e3870635d2f17961c51fc21f87abc9967cb05
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-18 23:45:00 +0600

    Project Info changed

--- src/App.js
+++ src/App.js
@@ -1,45 +1,55 @@
-import { createContext, useState, lazy, Suspense } from 'react';
-import './App.css';
+import { createContext, useState, lazy, Suspense } from "react";
+import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoute from './components/LogIn/PrivateRoute/PrivateRoute';
-import { Toaster } from 'react-hot-toast';
+import PrivateRoute from "./components/LogIn/PrivateRoute/PrivateRoute";
+import { Toaster } from "react-hot-toast";
 import { getDecodedUser } from "./components/LogIn/LogIn/LoginManager";
-import Preloader from './components/Shared/Preloader/Preloader';
-const NoMatch = lazy(() => import('./components/NoMatch'));
-const Home = lazy(() => import('./components/Home/Home/Home'));
-const LoginModal = lazy(() => import('./components/LogIn/LogIn/LoginModal'));
-const Dashboard = lazy(() => import('./components/Dashboard/Dashboard/Dashboard'));
-
+import Preloader from "./components/Shared/Preloader/Preloader";
+const NoMatch = lazy(() => import("./components/NoMatch"));
+const Home = lazy(() => import("./components/Home/Home/Home"));
+const LoginModal = lazy(() => import("./components/LogIn/LogIn/LoginModal"));
+const Dashboard = lazy(() =>
+  import("./components/Dashboard/Dashboard/Dashboard")
+);
 
 export const UserContext = createContext();
 const App = () => {
   const [admin, setAdmin] = useState(false);
   const [user, setUser] = useState(getDecodedUser());
-  const [selectedService, setSelectedService] = useState({})
+  const [selectedService, setSelectedService] = useState({});
 
   return (
-    <UserContext.Provider value={{user, setUser, admin, setAdmin, selectedService, setSelectedService}}>
+    <UserContext.Provider
+      value={{
+        user,
+        setUser,
+        admin,
+        setAdmin,
+        selectedService,
+        setSelectedService,
+      }}
+    >
       <Router>
-          <Toaster/>
-          <Suspense fallback={<Preloader/>}>
-            <Switch>
-              <PrivateRoute path="/dashboard">
-                <Dashboard/>
-              </PrivateRoute>
-              <Route path="/login">
-                <LoginModal/>
-              </Route>
-              <Route exact path="/">
-                <Home/>
-              </Route>
-              <Route exact path='*'>
-                <NoMatch/>
-              </Route>
-            </Switch>
-          </Suspense>
+        <Toaster />
+        <Suspense fallback={<Preloader />}>
+          <Switch>
+            <PrivateRoute path="/dashboard">
+              <Dashboard />
+            </PrivateRoute>
+            <Route path="/login">
+              <LoginModal />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="*">
+              <NoMatch />
+            </Route>
+          </Switch>
+        </Suspense>
       </Router>
     </UserContext.Provider>
   );
-}
+};
 
 export default App;
