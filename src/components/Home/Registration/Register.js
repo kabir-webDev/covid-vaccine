4a5764f3fade45cb0ebab85308baf99f5b61fed1
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-24 18:27:35 +0600

    Footer work Added

---
+++ src/components/Home/Registration/Register.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Register.css";
+function Register() {
+  return (
+    <section id="registration" className="rgtr">
+      <h4 className="miniTitle text-center">Register Here</h4>
+      <br />
+      <br />
+      <a className="atr" href="regi_form">
+        <span className="span-1"></span>
+        <span className="span-2"></span>
+        <span className="span-3"></span>
+        <span className="span-4"></span>
+        Registration
+      </a>
+    </section>
+  );
+}
+
+export default Register;
---
+++ src/components/Home/Registration/RegisterForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+function RegisterForm() {
+  return (
+    <section id="regi_form">
+      This is a form Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Enim obcaecati aspernatur dolorem id expedita ullam doloremque eos
+      perferendis minus facilis, tempora optio ipsa, officiis corrupti rerum
+      asperiores dolore, aliquam repellat! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Quod quisquam perspiciatis ab culpa totam
+      assumenda similique, distinctio deserunt impedit laudantium exercitationem
+      commodi minus qui, aspernatur amet eos atque quae officia. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Iusto expedita neque,
+      aperiam quae aspernatur, aut atque explicabo libero architecto consequatur
+      voluptas, earum porro! Molestias rem minus quidem nihil ipsam
+      mollitia?This is a form Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Enim obcaecati aspernatur dolorem id expedita ullam doloremque eos
+      perferendis minus facilis, tempora optio ipsa, officiis corrupti rerum
+      asperiores dolore, aliquam repellat! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Quod quisquam perspiciatis ab culpa totam
+      assumenda similique, distinctio deserunt impedit laudantium exercitationem
+      commodi minus qui, aspernatur amet eos atque quae officia. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Iusto expedita neque,
+      aperiam quae aspernatur, aut atque explicabo libero architecto consequatur
+      voluptas, earum porro! Molestias rem minus quidem nihil ipsam
+      mollitia?This is a form Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Enim obcaecati aspernatur dolorem id expedita ullam doloremque eos
+      perferendis minus facilis, tempora optio ipsa, officiis corrupti rerum
+      asperiores dolore, aliquam repellat! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Quod quisquam perspiciatis ab culpa totam
+      assumenda similique, distinctio deserunt impedit laudantium exercitationem
+      commodi minus qui, aspernatur amet eos atque quae officia. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Iusto expedita neque,
+      aperiam quae aspernatur, aut atque explicabo libero architecto consequatur
+      voluptas, earum porro! Molestias rem minus quidem nihil ipsam
+      mollitia?This is a form Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Enim obcaecati aspernatur dolorem id expedita ullam doloremque eos
+      perferendis minus facilis, tempora optio ipsa, officiis corrupti rerum
+      asperiores dolore, aliquam repellat! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Quod quisquam perspiciatis ab culpa totam
+      assumenda similique, distinctio deserunt impedit laudantium exercitationem
+      commodi minus qui, aspernatur amet eos atque quae officia. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Iusto expedita neque,
+      aperiam quae aspernatur, aut atque explicabo libero architecto consequatur
+      voluptas, earum porro! Molestias rem minus quidem nihil ipsam
+      mollitia?This is a form Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Enim obcaecati aspernatur dolorem id expedita ullam doloremque eos
+      perferendis minus facilis, tempora optio ipsa, officiis corrupti rerum
+      asperiores dolore, aliquam repellat! Lorem ipsum dolor sit amet
+      consectetur adipisicing elit. Quod quisquam perspiciatis ab culpa totam
+      assumenda similique, distinctio deserunt impedit laudantium exercitationem
+      commodi minus qui, aspernatur amet eos atque quae officia. Lorem ipsum
+      dolor sit amet consectetur, adipisicing elit. Iusto expedita neque,
+      aperiam quae aspernatur, aut atque explicabo libero architecto consequatur
+      voluptas, earum porro! Molestias rem minus quidem nihil ipsam mollitia?{" "}
+    </section>
+  );
+}
+
+export default RegisterForm;
