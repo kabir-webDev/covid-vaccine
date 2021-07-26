21b8bfcaff16045cd3abf7cc63a1366b41b825b1
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-26 21:12:39 +0600

    Registration Form

--- src/components/Home/Registration/RegisterForm.js
+++ src/components/Home/Registration/RegisterForm.js
@@ -5,23 +5,23 @@ function RegisterForm() {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors },
   } = useForm();
   const onSubmit = (data) => console.log(data);
 
-  console.log(watch("example"));
   return (
     <section id="regi_form">
       <form onSubmit={handleSubmit(onSubmit)}>
-        {/* register your input into the hook by invoking the "register" function */}
-        <input placeholder="test" {...register("example")} />
+        <input {...register("firstName")} />
+        <select {...register("gender")}>
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <input defaultValue="test" {...register("example")} />
 
-        {/* include validation with required or other standard HTML validation rules */}
         <input {...register("exampleRequired", { required: true })} />
-        {/* errors will return when field validation fails  */}
         {errors.exampleRequired && <span>This field is required</span>}
-
         <input type="submit" />
       </form>
     </section>
