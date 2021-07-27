6a02e8d9ef57a4b74aefa9b780aefeb508a1754b
Kabir Hasan <kabir.webdev@gmail.com>
2021-07-27 08:21:44 +0600

    Firebase Config Problem Solved

--- src/components/Home/Registration/RegisterForm.js
+++ src/components/Home/Registration/RegisterForm.js
@@ -1,7 +1,14 @@
 import React from "react";
+import GetData from "./GetData";
+import Links from "./Links";
 
 function RegisterForm() {
-  return <div>Form will be here</div>;
+  return (
+    <div>
+      {/* <Links /> */}
+      <GetData />
+    </div>
+  );
 }
 
 export default RegisterForm;
--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,8 +1,8 @@
-// import firebase from "firebase/app";
-import firebase from "firebase";
+import firebase from "firebase/app";
 import "firebase/firestore";
 
-const firebasecon = {
+// Your web app's Firebase configuration
+var firebaseConfig = {
   apiKey: "AIzaSyBlcce2AstAZjIv_VTHQPa_Cl0KT-5Ne4Q",
   authDomain: "co-vaxbd.firebaseapp.com",
   projectId: "co-vaxbd",
@@ -10,10 +10,10 @@ const firebasecon = {
   messagingSenderId: "1047100892698",
   appId: "1:1047100892698:web:474324aff3e366d368d3a3",
 };
+// Initialize Firebase
+const fb = firebase.initializeApp(firebaseConfig);
 
-firebase.initializeApp(firebasecon);
-
-export default firebase;
+export const db = fb.firestore();
 
 // var firebaseApp = firebase.initializeApp({
 //   apiKey: "AIzaSyBlcce2AstAZjIv_VTHQPa_Cl0KT-5Ne4Q",
